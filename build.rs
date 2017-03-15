6ac4b53f043c74a363743bc087d334e5040c2d9f
Stephen Sherratt <stephen@sherra.tt>
2017-03-16 02:26:46 +1100

    Test different EcsCtx configs

--- build.rs
+++ build.rs
@@ -5,9 +5,12 @@ extern crate statecs;
 fn main() {
     let config_a = statecs::Config::new();
     let config_b = statecs::Config { single_component_bitfield: false, .. config_a };
+    let config_c = statecs::Config { combine_flag_set: false, .. config_a };
 
     statecs::generate("ecs_0.toml", Path::new("src")
                       .join("generated/generated_0_a.rs"), config_a);
     statecs::generate("ecs_0.toml", Path::new("src")
                       .join("generated/generated_0_b.rs"), config_b);
+    statecs::generate("ecs_0.toml", Path::new("src")
+                      .join("generated/generated_0_c.rs"), config_c);
 }
--- src/generated/mod.rs
+++ src/generated/mod.rs
@@ -1,2 +1,3 @@
 pub mod generated_0_a;
 pub mod generated_0_b;
+pub mod generated_0_c;
